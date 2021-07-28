24d8ea303978b45b15c371990a0b040a0d06ebf2
Khoa Ho <khoaldho00@gmail.com>
2021-07-28 06:29:55 -0400

    fixed DB

---
+++ Database/MySQL/dumps/Dump20210728/marketplace_product.sql
@@ -0,0 +1,64 @@
+CREATE DATABASE  IF NOT EXISTS `marketplace` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `marketplace`;
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+--
+-- Host: localhost    Database: marketplace
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `is_removed` bit(1) NOT NULL DEFAULT b'0',
+  `product_name` varchar(255) NOT NULL,
+  `product_category` varchar(50) NOT NULL,
+  `product_description` text,
+  `product_image_link` text,
+  `unit_price` decimal(65,2) NOT NULL,
+  `initial_quantity` int NOT NULL DEFAULT '1',
+  `remaining_quantity` int DEFAULT NULL,
+  `seller_username` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`product_id`),
+  UNIQUE KEY `product_id_UNIQUE` (`product_id`),
+  KEY `fk_seller_user_name_idx` (`seller_username`),
+  CONSTRAINT `fk_seller_username` FOREIGN KEY (`seller_username`) REFERENCES `user` (`username`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,_binary '\0','x','automobile','asadsa',NULL,1.00,1,1,NULL),(2,_binary '\0','x','automobile','aaa',NULL,1.00,2,3,NULL),(3,_binary '\0','furniture1','Furniture','asa','uploads\\img\\1627253600167flash.jpg',1.00,1,1,NULL),(4,_binary '\0','furniture2','Furniture','adxzcx','uploads\\img\\1627253694784StaticElectricity.png',2.00,2,2,NULL),(5,_binary '\0','furniture3','Furniture','f3','uploads\\img\\1627254260666magicarp.jpg',3.00,3,3,NULL),(6,_binary '\0','furniture4','Furniture','f4','',4.00,4,4,NULL),(7,_binary '\0','electronics1','electronics','E1','uploads\\img\\1627281088326StaticElectricity.png',1.00,1,1,'user2'),(8,_binary '\0','electronics2','furniture','affsadff electronics','uploads\\img\\1627332362234StaticElectricity.png',20.00,1,1,'user3'),(9,_binary '\0','new electronics','electronics','fasdasd elec','uploads\\img\\1627339832164StaticElectricity.png',100.00,2,2,'user4'),(10,_binary '\0','new furniture','furniture','asdaxzc','uploads\\img\\1627340506862MySpectrum.jpg',100.00,5,5,'user1'),(11,_binary '\0','iCar','furniture','iCar','uploads\\img\\1627411909538Elements.jpg',100000.00,1,1,'user1'),(12,_binary '\0','','','','',0.00,1,1,NULL),(13,_binary '\0','','','','',0.00,1,1,NULL),(14,_binary '\0','','','','',0.00,1,1,NULL),(15,_binary '\0','batcar','automobile','car of batman','uploads\\img\\1627466378592batcarmodel.jpg',999999.00,1,1,'user1');
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-28  6:27:51
